070ba8ff2a29d32ea63517c632ea13892b9da052
Mingyu Hsia <myhsia@outlook.com>
2026-04-03 14:09:41 +0800

    Optimized the lua script

--- source/build.lua
+++ source/build.lua
@@ -12,18 +12,28 @@ local moments_list = {
   "03-parameter_regimes/03-fast_bath",
   "03-parameter_regimes/04-slow_bath",
   "03-parameter_regimes/05-lowT",
-  "03-parameter_regimes/05-lowT",
+  "03-parameter_regimes/05-llowT",
   "03-parameter_regimes/06-highT",
+  "03-parameter_regimes/06-hhighT",
   "03-parameter_regimes/07-weak_coupling",
   "03-parameter_regimes/08-strong_coupling"
 }
 
-function run(dir, cmd)
-  return os.execute("cd " .. dir .. ";" .. cmd)
+local function format_time(seconds)
+  local hours = math.floor(seconds / 3600)
+  local mins = math.floor((seconds % 3600) / 60)
+  local secs = seconds % 60
+  return string.format("%02d:%02d:%02d", hours, mins, secs)
 end
 
+local start_overall = os.time()
 for i, path in ipairs(moments_list) do
   print(string.format("[%d/%d] Calculating Moments", i, #moments_list))
   local _path = path .. "/moments/"
   os.execute("cd " .. _path .. "; python main.py")
+  print()
 end
+local end_overall = os.time()
+
+local elapsed = os.difftime(end_overall, start_overall)
+print("All tasks completed.\nTotal running time: " .. format_time(elapsed))
